1a3a3e04d53502dd7a63f3f177434d6c2f461c64
Reiley Meeks <rmeeks10@icloud.com>
2021-12-02 12:54:21 -0500

    Updated File

--- MDP.py
+++ MDP.py
@@ -1,15 +1,13 @@
 # Reiley Meeks
 # Intro AI
-# DD/MM/YYYY
-# 1/12/2021
-# Go to line 191 and below for my code.
+# 2nd December 2021
+# My code starts at 187
 
-import numpy as np
 import copy
+import numpy as np
+
 
 # Given Code
-
-
 def getIndexOfState(S, x, y):
     """ Get the index of the state that contains the (x,y) position in S
 
@@ -28,8 +26,7 @@ def getIndexOfState(S, x, y):
 
 
 def getPolicyForGrid(S, U, A, P, i_terminal_states):
-    """ Computes the policy as a list of characters indicating which direction to
-move in at the state
+    """ Computes the policy as a list of characters indicating which direction to move in at the state
 
     Parameters:
         S (list): States
@@ -73,31 +70,31 @@ move in at the state
             if i_left != -1:
                 i_states.append(i_left)
 
-            # Append the state itself to consider the agent bouncing off the
+            # Append the state itself to consider the agent bouncing off the boundary
+            i_states.append(i_s)
 
-    i_states.append(i_s)
+            # Calculate the expected utilities for each action in the state
+            i_a_max_eu = 0
+            max_eu = -100000  # don't wait to loop for i_a=0...
+            for i_a, a in enumerate(A):
 
-    # Calculate the expected utilities for each action in the state
-    i_a_max_eu = 0
-    max_eu = -100000  # don't wait to loop for i_a=0...
-    for i_a, a in enumerate(A):
-        # Get the expected utility for an action
-        eu = 0
+                # Get the expected utility for an action
+                eu = 0
+                for i_neighbor in i_states:
+                    u_s_prime = U[i_neighbor]
+                    prob_s_prime = P[i_a, i_s, i_neighbor]
+                    eu += (prob_s_prime * u_s_prime)
 
-    for i_neighbor in i_states:
-        u_s_prime = U[i_neighbor]
-        prob_s_prime = P[i_a, i_s, i_neighbor]
-        eu += (prob_s_prime * u_s_prime)
-    # Check if max expected utility
-    if eu > max_eu:
-        max_eu = eu
-        i_a_max_eu = i_a
+                # Check if max expected utility
+                if eu > max_eu:
+                    max_eu = eu
+                    i_a_max_eu = i_a
 
-    # Set the action character
-    action = A[i_a_max_eu]
+            # Set the action character
+            action = A[i_a_max_eu]
 
-    # Add the action to the policy
-    policy.append(action)
+        # Add the action to the policy
+        policy.append(action)
 
     return policy
 
@@ -111,8 +108,7 @@ def printPolicyForGrid(policy, w, h, i_obs):
         '0' elements are obstacles in the grid.
 
     Parameters:
-        policy (list): 1d list of characters indicating which action to take for
-each state
+        policy (list): 1d list of characters indicating which action to take for each state
         w (int): width of the grid
         h (int): height of the grid
         i_obs(list): list of indices where obstacles are located
@@ -124,6 +120,7 @@ each state
     # Insert 0's for obstacle tiles
     for i_ob in i_obs:
         policy.insert(i_ob, '0')
+
         # Blank line to isolate the policy
     print('\n')
 
@@ -133,12 +130,6 @@ each state
         print(row)
 
 
-# P is the transition model matrix for the 4x3 grid world problem
-# P gets converted to a numpy array after it is hard-coded here
-# actions are in order: up, right, down, left
-# rows -> s
-# cols -> s'
-# [action, state, outcome], [a, s, s']
 P = [[[0.1, 0.1, 0., 0., 0.8, 0., 0., 0., 0., 0., 0.],
       [0.1, 0.8, 0.1, 0., 0., 0., 0., 0., 0., 0., 0.],
       [0., 0.1, 0., 0.1, 0., 0.8, 0., 0., 0., 0., 0.],
@@ -150,6 +141,7 @@ P = [[[0.1, 0.1, 0., 0., 0.8, 0., 0., 0., 0., 0., 0.],
       [0., 0., 0., 0., 0., 0., 0., 0.1, 0.8, 0.1, 0.],
       [0., 0., 0., 0., 0., 0., 0., 0., 0.1, 0.8, 0.1],
       [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.1, 0.9]],
+
      [[0.1, 0.8, 0., 0., 0.1, 0., 0., 0., 0., 0., 0.],
       [0., 0.2, 0.8, 0., 0., 0., 0., 0., 0., 0., 0.],
       [0., 0., 0.1, 0.8, 0., 0.1, 0., 0., 0., 0., 0.],
@@ -161,6 +153,7 @@ P = [[[0.1, 0.1, 0., 0., 0.8, 0., 0., 0., 0., 0., 0.],
       [0., 0., 0., 0., 0., 0., 0., 0., 0.2, 0.8, 0.],
       [0., 0., 0., 0., 0., 0.1, 0., 0., 0., 0.1, 0.8],
       [0., 0., 0., 0., 0., 0., 0.1, 0., 0., 0., 0.9]],
+
      [[0.9, 0.1, 0., 0., 0., 0., 0., 0., 0., 0., 0.],
       [0.1, 0.8, 0.1, 0., 0., 0., 0., 0., 0., 0., 0.],
       [0., 0.1, 0.8, 0.1, 0., 0., 0., 0., 0., 0., 0.],
@@ -172,6 +165,7 @@ P = [[[0.1, 0.1, 0., 0., 0.8, 0., 0., 0., 0., 0., 0.],
       [0., 0., 0., 0., 0., 0., 0., 0.1, 0.8, 0.1, 0.],
       [0., 0., 0., 0., 0., 0.8, 0., 0., 0.1, 0., 0.1],
       [0., 0., 0., 0., 0., 0., 0.8, 0., 0., 0.1, 0.1]],
+
      [[0.9, 0., 0., 0., 0.1, 0., 0., 0., 0., 0., 0.],
       [0.8, 0.2, 0., 0., 0., 0., 0., 0., 0., 0., 0.],
       [0., 0.8, 0.1, 0., 0., 0.1, 0., 0., 0., 0., 0.],
@@ -185,20 +179,23 @@ P = [[[0.1, 0.1, 0., 0., 0.8, 0., 0., 0., 0., 0., 0.],
       [0., 0., 0., 0., 0., 0., 0.1, 0., 0., 0.8, 0.1]]]
 P = np.array(P)
 
-# End Give Code
+
+# End Given Code
 
 
+# My Code
 class State:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
-def valueIteration(S, A, P, R_sates, discount, reward_threshold):
+def valueIterations(S, A, TM, R_states, discount, terminal_index_reward_pairs):
     U_prime = np.full(len(S), 0.0)
-    U_prime[6] = -1.0
-    U_prime[10] = 1.0
+    U_prime[6] = -1.
+    U_prime[10] = 1.
     threshold = 0.00000001
+
     delta = .1
 
     while delta > threshold:
@@ -206,37 +203,32 @@ def valueIteration(S, A, P, R_sates, discount, reward_threshold):
         delta = 0.0
 
         for i in range(len(S)):
-            if i != reward_threshold[0] and i != reward_threshold[1]:
-                U_prime[i] = R_sates[i] + (discount * getExpectedUtility(A, i, P, U, S))
+            if i != terminal_index_reward_pairs[0] and i != terminal_index_reward_pairs[1]:
+                U_prime[i] = R_states[i] + (discount * getExpectedUtility(A, i, TM, U, S))
                 if abs(U_prime[i] - U[i]) > delta:
                     delta = abs(U_prime[i] - U[i])
-
     return U
 
 
-def getExpectedUtility(action, state, P, uVal, setOfStates):
-    util = []
+def getExpectedUtility(action, state, TM, U_value, set_of_states):
+    x = []
 
     for i in range(len(action)):
-        total = 0
+        total = 0.0
+        for j in range(len(set_of_states)):
+            total += TM[i][state][j] * U_value[j]
+        x.append(total)
 
-        for j in range(len(setOfStates)):
-            total += (P[i][state][j] * uVal[j])
-        util.append(total)
-
-    return max(util)
+    return max(x)
 
 
 def main():
-    # Create the states
     states = [State(1, 1), State(1, 2), State(1, 3), State(1, 4),
               State(2, 1), State(2, 3), State(2, 4),
               State(3, 1), State(3, 2), State(3, 3), State(3, 4)]
 
-    # Define the action
-    action = ['u', 'r', 'd', 'l']
+    actions = ['u', 'r', 'd', 'l']
 
-    # Define the rewards in an np array
     rewards = np.array([-.04, -.04, -.04, -.04,
                         -.04, -.04, -1,
                         -.04, -.04, -.04, 1])
@@ -245,13 +237,15 @@ def main():
 
     terminal = [6, 10]
 
-    # Run valueIteration and store in numpy array result
-    result = np.array(valueIteration(states, action, P, rewards, discount, terminal))
-    print(result)  # Print result
+    values = np.array(valueIterations(states, actions, P, rewards, discount, terminal))
+    print(values)
 
-    # Store the getPolicyForGrid in poly then print
-    poly = getPolicyForGrid(states, result, action, P, terminal)
+    print()
+
+    poly = getPolicyForGrid(states, values, actions, P, terminal)
     print("Policy: %s" % poly)
+
+    printPolicyForGrid(poly, 4, 3, [5])
 
 
 if __name__ == '__main__':
